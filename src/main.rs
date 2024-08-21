b001a0a551976dc25feb3aa61e7f0ed509761994
dibkb <dibas9110@gmail.com>
2024-08-22 00:44:36 +0530

    string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,28 @@
-fn main() {
-    let x: i64 = 4000;
-    println!("{}",x);
+fn practice_bool(){
+    let age:u32 = 23;
+    let is_adult = age >= 18;
+    if is_adult {
+        println!("You are an adult.");
+    } else {
+        println!("You are not an adult.");
+    }
 }
+fn practice_string(){
+    let str = String::from("Hello");
+    let str: String = extract_first_word(str);
+    println!("{}",str)
+}
+fn extract_first_word(sentence:String)->String{
+    let mut ans:String = String::new();
+    for char in sentence.chars(){
+        if char == ' ' {
+            break;
+        }
+        ans.push_str(char.to_string().as_str())
+    }
+    return ans;
+}
+fn main() {
+    practice_string();
+}
+
