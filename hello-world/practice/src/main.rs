5aab047da563ddc6c4d335d99acea7e3603452ed
dibkb <dibas9110@gmail.com>
2025-05-18 20:43:16 +0530

    strunct

--- hello-world/practice/src/main.rs
+++ hello-world/practice/src/main.rs
@@ -1,9 +1,22 @@
-fn main() {
-    let mut sentence = String::from("Hello world");
-    println!("{}", append_rust_suffix(&mut sentence));
+struct User {
+    first_name : String,
+    last_name : String,
+    email : String,
+}
+fn print_user_info(user:&User){
+    println!("First Name : {}",user.first_name);
+    println!("Last Name : {}",user.last_name);
+    println!("Email : {}",user.email);
 }
 
-fn append_rust_suffix(sen: &mut String) -> &mut String {
-    sen.push_str(" from rust");
-    sen
+fn main() {
+    let user_one: User = User{
+        first_name : String::from("Ramesh"),
+        last_name : String::from("Gol"),
+        email : String::from("ramesh@mail.com"),
+    };
+
+    print_user_info(&user_one);
 }
+
+
