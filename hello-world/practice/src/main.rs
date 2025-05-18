b67376ceeadeba15ac5663f1b7e740cbf8baf1c3
dibkb <dibas9110@gmail.com>
2025-05-18 13:03:19 +0530

    string

--- hello-world/practice/src/main.rs
+++ hello-world/practice/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello rust");
+    let sentence = "Hello";
+    let mut ans = String::new();
+    for (_,c) in sentence.chars().enumerate(){
+        if c != ' ' {
+            ans.push(c);
+        }else{
+            break;
+        }
+    }
+    println!("{}",ans);
 }
