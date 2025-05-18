166667f1746cfd79bfd9fe4b18387b9fbc11e997
dibkb <dibas9110@gmail.com>
2025-05-18 21:16:39 +0530

    matching

--- hello-world/practice/src/main.rs
+++ hello-world/practice/src/main.rs
@@ -1,22 +1,21 @@
-struct User {
-    first_name : String,
-    last_name : String,
-    email : String,
-}
-fn print_user_info(user:&User){
-    println!("First Name : {}",user.first_name);
-    println!("Last Name : {}",user.last_name);
-    println!("Email : {}",user.email);
+enum Shape{
+    Square(f64),
+    Circle(f64),
+    Rect(f64,f64)
 }
 
+fn calculate_area(shape: &Shape)-> f64{
+    let pi = std::f64::consts::PI;
+    match shape {
+        Shape::Circle(radius)=> radius * radius *  pi,
+        Shape::Square(a)=> a * a,
+        Shape::Rect(a,b)=> a*b
+    }
+}
 fn main() {
-    let user_one: User = User{
-        first_name : String::from("Ramesh"),
-        last_name : String::from("Gol"),
-        email : String::from("ramesh@mail.com"),
-    };
-
-    print_user_info(&user_one);
+    let square = Shape::Square(32.21);
+    println!("Area of the square with side {} is {}", 32.21, calculate_area(&square));
+    
 }
 
 
